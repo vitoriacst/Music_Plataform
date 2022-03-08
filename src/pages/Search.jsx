f7b2022935d05b0f0ee6cc1b2c093962ece925e6
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-03-08 00:43:40 -0300

    requisito 5 com erros de Lint

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,13 +2,54 @@ import React from 'react';
 import Header from './Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      procura: '',
+      Button: true,
+
+    };
+  }
+
+  change = (event) => {
+    const valueCheck = 2;
+    this.setState(() => ({
+      procura: event.target.value,
+      Button: event.target.value.length < valueCheck,
+    }));
+  }
+
   render() {
+    const {
+      Button,
+    } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <h1>search</h1>
+        <form action="submit">
+          <input
+            type="text"
+            name="procura"
+            id="Artista"
+            data-testid="search-artist-input"
+            placeholder="Explore"
+            onChange={ this.change }
+
+          />
+          <button
+            data-testid="search-artist-button"
+            type="submit"
+            name="Button"
+            disabled={ Button }
+
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
 }
+Search.propTypes = {
+};
 export default Search;
