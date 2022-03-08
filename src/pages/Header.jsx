e2bc6ce345d7e48f8d0e225765324736f9d2c7f5
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-03-07 23:50:37 -0300

    requisito 04

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,16 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+// import Load from './Load';
+// import Login from './Login';
 
 class Header extends React.Component {
+  // constructor() {
+  //   super()
+  //   const{
+  //   }= this.state
+  // };
+
+  // RequirimentApi=(event) => {
+  //   event.preventDefault();
+  //   const {Load} = this.state;
+  //   this.setState({ load: true },
+  //     async () => {
+  //       await createUser({Load});
+  //       this.setState({
+  //         load: false,
+  //       });
+  //     });
+  // }
+
   render() {
     return (
       <div>
         <header data-testid="header-component">
           <Link to="/">Login</Link>
-          <Link to="/Album">Album</Link>
-          <Link to="/Profile">Profile</Link>
-          <Link to="/Search">Search</Link>
-          <Link to="/Favorites">Favorites</Link>
+          <Link to="/album">Album</Link>
+          <Link to="/profile" data-testid="link-to-profile">Profile</Link>
+          <Link to="/search" data-testid="link-to-search">Search</Link>
+          <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
           <Link to="/profile/edit">ProfileEdit</Link>
         </header>
       </div>
