8220e22e565c1f658d19ef759dced5d89e1cc0cf
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-03-09 11:07:10 -0300

    requisito 6 finalmenteeee aaaa

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from './Header';
 import Load from './Load';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends React.Component {
   constructor() {
@@ -8,6 +10,10 @@ class Search extends React.Component {
     this.state = {
       Button: true,
       load: false,
+      album: [],
+      foundAlbum: false,
+      name: '',
+      Salva: '',
 
     };
   }
@@ -16,13 +22,34 @@ class Search extends React.Component {
     const valueCheck = 2;
     this.setState(() => ({
       Button: event.target.value.length < valueCheck,
+      name: event.target.value,
     }));
   }
+
+  Button= async (event) => {
+    event.preventDefault();
+    const { name } = this.state;
+    this.setState({
+      load: true,
+      Salva: name,
+    });
+    const GuardaAlbuns = await searchAlbumsAPI(name);
+    this.setState({
+      load: false,
+      album: GuardaAlbuns,
+      foundAlbum: true,
+      name: '',
+    });
+  };
 
   render() {
     const {
       Button,
       load,
+      album,
+      foundAlbum,
+      name,
+      Salva,
     } = this.state;
 
     return (
@@ -33,21 +60,47 @@ class Search extends React.Component {
             <form action="submit">
               <input
                 type="text"
-                name="procura"
+                name="name"
                 id="Artista"
                 data-testid="search-artist-input"
                 placeholder="Explore"
                 onChange={ this.change }
+                value={ name }
               />
               <button
                 data-testid="search-artist-button"
                 type="submit"
                 name="Button"
                 disabled={ Button }
+                onClick={ this.Button }
+                value={ name }
               >
                 Pesquisar
               </button>
             </form>
+            {
+              foundAlbum ? (<h1>{`Resultado de álbuns de: ${Salva}`}</h1>) : null
+            }
+            {album.length <= 0 && !name ? <p>Nenhum álbum foi encontrado</p>
+              : album.map((i) => (
+                <Link
+                  key={
+                    album.collectionId
+                  }
+                  data-testid={ `link-to-album-${i.collectionId}` }
+                  to={ `/album/${i.collectionId}` }
+                >
+                  <div key={ album.collectionId }>
+                    {' '}
+                    <img
+                      src={ i.artworkUrl100 }
+                      alt={ `Capa do i ${i.collectionName}` }
+                    />
+                    <p>{ i.collectionName }</p>
+                    <p>{ i.artistName }</p>
+                  </div>
+                </Link>
+              ))}
           </>
         )}
       </div>
--- src/services/searchAlbumsAPI.js
+++ src/services/searchAlbumsAPI.js
@@ -1,7 +1,7 @@
 const searchAlbumsAPI = async (artist) => {
   const artistNameURL = encodeURI(artist).replaceAll('%20', '+');
 
-  const getAlbumsAPI = `https://itunes.apple.com/search?entity=album&term=${artistNameURL}&attribute=allArtistTerm`;
+  const getAlbumsAPI = `https://itunes.apple.com/search?entity=album&term=${artistNameURL}`;
 
   const APIResponse = await fetch(getAlbumsAPI);
 
